11f96750fecf60d2fc50495d1b1cb422ca91bda5
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2024-10-10 17:29:51 +0200

    cuda wsl mehhh
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- driver/docker-container/devices_wip.go
+++ driver/docker-container/devices_wip.go
@@ -12,6 +12,8 @@ func (d *Driver) createDevice(ctx context.Context, dev string, l progress.SubLog
 	switch dev {
 	case "docker.com/gpu":
 		return d.createVenusGPU(ctx, l)
+	case "nvidia.com/gpu":
+		return d.createNvidiaGPU(ctx, l)
 	default:
 		return errors.Errorf("unsupported device %q, loading custom devices not implemented yet", dev)
 	}
@@ -37,6 +39,117 @@ devices:
     # make this dynamic
     - path: /dev/dri/card0
     - path: /dev/dri/renderD128
+EOF
+`
+
+	stdout := &bytes.Buffer{}
+	stderr := &bytes.Buffer{}
+	err := d.run(ctx, []string{"/bin/ash"}, bytes.NewReader([]byte(script)), stdout, stderr)
+	if err != nil {
+		l.Log(1, stdout.Bytes())
+		l.Log(2, stderr.Bytes())
+		return err
+	}
+	return nil
+}
+
+func (d *Driver) createNvidiaGPU(ctx context.Context, l progress.SubLogger) error {
+	script := `#!/bin/sh
+set -e
+mkdir -p /etc/cdi
+cat <<EOF > /etc/cdi/nvidia-gpu.yaml
+---
+cdiVersion: 0.3.0
+containerEdits:
+  env:
+  - NVIDIA_VISIBLE_DEVICES=void
+  hooks:
+  - args:
+    - nvidia-cdi-hook
+    - create-symlinks
+    - --link
+    - /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/nvidia-smi::/usr/bin/nvidia-smi
+    hookName: createContainer
+    path: /usr/bin/nvidia-cdi-hook
+  - args:
+    - nvidia-cdi-hook
+    - update-ldcache
+    - --folder
+    - /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9
+    - --folder
+    - /usr/lib/wsl/lib
+    hookName: createContainer
+    path: /usr/bin/nvidia-cdi-hook
+  mounts:
+  - containerPath: /usr/lib/wsl/lib/libdxcore.so
+    hostPath: /usr/lib/wsl/lib/libdxcore.so
+    options:
+    - ro
+    - nosuid
+    - nodev
+    - bind
+  - containerPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libcuda.so.1.1
+    hostPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libcuda.so.1.1
+    options:
+    - ro
+    - nosuid
+    - nodev
+    - bind
+  - containerPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libcuda_loader.so
+    hostPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libcuda_loader.so
+    options:
+    - ro
+    - nosuid
+    - nodev
+    - bind
+  - containerPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libnvdxgdmal.so.1
+    hostPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libnvdxgdmal.so.1
+    options:
+    - ro
+    - nosuid
+    - nodev
+    - bind
+  - containerPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libnvidia-ml.so.1
+    hostPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libnvidia-ml.so.1
+    options:
+    - ro
+    - nosuid
+    - nodev
+    - bind
+  - containerPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libnvidia-ml_loader.so
+    hostPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libnvidia-ml_loader.so
+    options:
+    - ro
+    - nosuid
+    - nodev
+    - bind
+  - containerPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libnvidia-ptxjitcompiler.so.1
+    hostPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/libnvidia-ptxjitcompiler.so.1
+    options:
+    - ro
+    - nosuid
+    - nodev
+    - bind
+  - containerPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/nvcubins.bin
+    hostPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/nvcubins.bin
+    options:
+    - ro
+    - nosuid
+    - nodev
+    - bind
+  - containerPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/nvidia-smi
+    hostPath: /usr/lib/wsl/drivers/nvmdi.inf_amd64_978a3b585e321cd9/nvidia-smi
+    options:
+    - ro
+    - nosuid
+    - nodev
+    - bind
+devices:
+- containerEdits:
+    deviceNodes:
+    - path: /dev/dxg
+  name: all
+kind: nvidia.com/gpu
 EOF
 `
 
