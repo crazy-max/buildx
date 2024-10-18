dfd5ccf0ab38b09fc24082562c785e3713e30d12
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2024-10-18 12:26:27 +0200

    config: fix file/folder ownership
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- util/confutil/node.go
+++ util/confutil/node.go
@@ -10,23 +10,24 @@ import (
 
 var nodeIdentifierMu sync.Mutex
 
-func TryNodeIdentifier(configDir string) (out string) {
+func TryNodeIdentifier(cfg *Config) (out string) {
 	nodeIdentifierMu.Lock()
 	defer nodeIdentifierMu.Unlock()
-	sessionFile := filepath.Join(configDir, ".buildNodeID")
-	if _, err := os.Lstat(sessionFile); err != nil {
+	sessionFilename := ".buildNodeID"
+	sessionFilepath := filepath.Join(cfg.Dir(), sessionFilename)
+	if _, err := os.Lstat(sessionFilepath); err != nil {
 		if os.IsNotExist(err) { // create a new file with stored randomness
 			b := make([]byte, 8)
 			if _, err := rand.Read(b); err != nil {
 				return out
 			}
-			if err := os.WriteFile(sessionFile, []byte(hex.EncodeToString(b)), 0600); err != nil {
+			if err := cfg.WriteFile(sessionFilename, []byte(hex.EncodeToString(b)), 0600); err != nil {
 				return out
 			}
 		}
 	}
 
-	dt, err := os.ReadFile(sessionFile)
+	dt, err := os.ReadFile(sessionFilepath)
 	if err == nil {
 		return string(dt)
 	}
