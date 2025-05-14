e361a96d288ef2db05757bffe90a894fb5363133
CrazyMax <1951866+crazy-max@users.noreply.github.com>
2025-05-14 20:05:28 +0200

    test
    Signed-off-by: CrazyMax <1951866+crazy-max@users.noreply.github.com>

--- tests/build.go
+++ tests/build.go
@@ -519,7 +519,7 @@ func testBuildProgress(t *testing.T, sb integration.Sandbox) {
 	// progress=tty
 	cmd := buildxCmd(sb, withArgs("build", "--progress=tty", "--output=type=cacheonly", dir))
 	f, err := pty.StartWithSize(cmd, &pty.Winsize{
-		Cols: 80,
+		Cols: 120,
 		Rows: 24,
 	})
 	require.NoError(t, err)
